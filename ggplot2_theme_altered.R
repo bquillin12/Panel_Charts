b29ec1c35e438255bd05cf75191eed6a45ef551d
Bryce Quillin <bquillin@yahoo.com>
2019-06-17 16:06:37 -0400

    Add files via upload

---
+++ ggplot2_theme_altered.R
@@ -0,0 +1,72 @@
+################################################
+### ggplot2 theme for Pfizer Global Economics
+#############################################
+
+#packages
+library(grid)
+library(ggthemes)
+library(ggplot2)
+library(gridExtra)
+
+# theme function
+theme_Publication2 <- function(base_size=14, base_family="Georgia") {
+  library(grid)
+  library(ggthemes)
+  (theme_foundation(base_size=base_size, base_family=base_family)
+    + theme(plot.title = element_text(face = "bold",
+                                      size = 8, hjust = 0.5),
+            plot.subtitle = element_text(size = 7),
+            plot.caption = element_text(size = 7),
+            text = element_text(),
+            panel.background = element_rect(colour = NA),
+            plot.background = element_rect(colour = NA),
+            panel.border = element_rect(colour = NA),
+            axis.title = element_text(face = "bold",size = rel(1)),
+            axis.title.y = element_text(angle=90,vjust =2),
+            axis.text.x = element_text(angle = 45, hjust = 1, size = 6),
+            axis.text.y = element_text(size =6),
+            axis.text = element_text(), 
+            axis.line = element_line(colour="black"),
+            axis.ticks = element_line(),
+            panel.grid.major = element_line(colour="#f0f0f0"),
+            panel.grid.minor = element_blank(),
+            legend.key = element_rect(colour = NA),
+            legend.position = "bottom",
+            legend.direction = "horizontal",
+            legend.key.size= unit(0.2, "cm"),
+            #legend.margin = unit(0, "cm"),
+            legend.title = element_blank(),
+            legend.text = element_text(size = 6),
+            plot.margin=unit(c(10,5,5,5),"mm"),
+            strip.background=element_rect(colour="#f0f0f0",fill="#f0f0f0"),
+            strip.text = element_text(face="bold")
+    ))
+  
+}
+
+scale_fill_Publication <- function(...){
+  library(scales)
+  discrete_scale("fill","Publication",manual_pal(values = c("#386cb0","#fdb462","#7fc97f","#ef3b2c","#662506","#a6cee3","#fb9a99","#984ea3","#ffff33")), ...)
+  
+}
+
+scale_colour_Publication <- function(...){
+  library(scales)
+  discrete_scale("colour","Publication",manual_pal(values = c("#386cb0","#fdb462","#7fc97f","#ef3b2c","#662506","#a6cee3","#fb9a99","#984ea3","#ffff33")), ...)
+  
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
